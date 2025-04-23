5281c69025884cee0250b9661549b88c452678a1
João Tambue <tjoao7279@gmail.com>
2025-04-23 04:26:51 +0100

    primeiro commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,4 @@
+export default function About() {
+    return <h1 className="text-2xl">Sobre o Projeto</h1>;
+  }
+  
---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+      <main className="flex items-center justify-center h-screen">
+        <div className="w-[440px] h-[600px] grid grid-rows-[min-content_1fr_min-content] bg-white shadow-xl rounded-sm p-4">
+          <div className="flex justify-between items-center gap-4 bg-zinc-500 h-[30px]">
+            <h1>Notificações</h1>
+            <p>MARCAR TODAS COMO LIDAS</p>
+          </div>
+
+          <div>Recentes</div>
+
+          <div>
+            <div>
+                
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+  
